5e078708832f94e99d623b99f55065188adb3c61
ginrou799 <ginrou799@gmail.com>
2014-05-10 09:24:26 +0900

    WIP - マウス操作にTrackballControls.jsを追加
    - オブジェクト周りはまだ実装できてない

--- viewer.js
+++ viewer.js
@@ -1,80 +1,75 @@
-var main = function(){
-    console.log("hello world");
-    var scene = new THREE.Scene();
-    var width  = 600;
-    var height = 400;
-    var fov    = 60;
-    var aspect = width / height;
-    var near   = 1;
-    var far    = 1000;
-    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+(function(){
 
-    var camera_angle = {theta: 0, psi: 0};
-    var camera_distance = 15;
+    var scene;
+    var camera;
+    var controls;
+    var renderer;
 
-    camera.position.set( camera_distance * Math.cos(camera_angle.theta),
-			 camera_distance * Math.sin(camera_angle.theta),
-			 camera_distance * Math.sin(camera_angle.psi));
+    var gen_circle = function(radius){
+	if(typeof radius === 'undefined') radius = 1.0;
+	
+	var material = new THREE.MeshBasicMaterial({color:0x0000ff});
+	var circleGeometry = new THREE.CircleGeometry(circleGeometry, material);
+	return new THREE.Mesh(circleGeometry, material);
+    };
 
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    var add_object = function(scene, pos) {
+	var sphere = new THREE.Mesh(
+	    new THREE.TorusKnotGeometry(0.6, 0.2, 128, 32, 2, 3),
+	    new THREE.MeshPhongMaterial({color:0xff0000})
+	);
+	scene.add(sphere);
 
-    var axis = new THREE.AxisHelper(100);
-    scene.add(axis);
+	circle = gen_circle();
+	circle.position = new THREE.Vector3(pos[0], pos[1], pos[2]);
+	scene.add(circle);
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-    document.body.appendChild(renderer.domElement);
+    };
 
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(0, 0.7, 0.7 );
-    scene.add(directionalLight);
+    var init = function() {
 
-    var sphere = new THREE.Mesh(
-	new THREE.TorusKnotGeometry(0.6, 0.2, 128, 32, 2, 3),
-	new THREE.MeshPhongMaterial({color:0xff0000})
-    );
-    scene.add(sphere);
+	scene = new THREE.Scene();
 
+	var width  = 600;
+	var height = 400;
+	var fov    = 60;
+	var aspect = width / height;
+	var near   = 1;
+	var far    = 1000;
 
-    // var circleGeometry = new THREE.CircleGeometry(2, 128);
-    // circleGeometry.vertices.shift();
-    // var circleMaeterial = new THREE.LineBasicMaterial({color:0x00ff00});
-    // var xCircle = new THREE.Line(circleGeometry, circleMaeterial);
-    // scene.add(xCircle);
-
-    // var yCircle = new THREE.Line(circleGeometry, circleMaeterial);
-    // yCircle.rotation.set( Math.Pi/2.0, 0, 0);
-    // scene.add(yCircle);
-
-    renderer.render(scene, camera);
-
-    // mouse rotation
-    var mousedown = false;
-    var prevPoint;
-    renderer.domElement.addEventListener('mousedown', function(e){
-	mousedown = true;
-	prevPoint = {x:e.x, y:e.y};
-    }, false);
-
-    renderer.domElement.addEventListener('mousemove', function(e){
-	if(!mousedown) return;
-
-	camera_angle.theta += 0.01 * (prevPoint.x - e.x);
-	camera_angle.psi += 0.01 * (prevPoint.y - e.y);
-	camera.position.set( camera_distance * Math.cos(camera_angle.theta),
-			     camera_distance * Math.sin(camera_angle.theta),
-			     camera_distance * Math.sin(camera_angle.psi));
+	camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.set( 20, 20, 20);
+	camera.up.set(0, 0, 1);
 	camera.lookAt(new THREE.Vector3(0,0,0));
 
-	prevPoint = {x:e.x, y:e.y};
+	controls = new THREE.TrackballControls(camera);
+
+	var axis = new THREE.AxisHelper(100);
+	scene.add(axis);
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+
+	var directionalLight = new THREE.DirectionalLight(0xffffff);
+	directionalLight.position.set(0, 0.7, 0.7 );
+	scene.add(directionalLight);
+
+	add_object(scene, renderer, [1,2,3]);
+
+    }
+
+    var mainLoop = function() {
+	requestAnimationFrame(mainLoop);
+	controls.update();
+	renderer.clear();
 	renderer.render(scene, camera);
-    }, false);
+    }
 
-    renderer.domElement.addEventListener('mouseup', function(e){
-	mousedown = false;
-    }, false);
+    var main = function(){
+	init();
+	mainLoop();
+    };
 
-};
-
-window.addEventListener( 'DOMContentLoaded', main, false );
-
+    window.addEventListener( 'DOMContentLoaded', main, false );
+})();
